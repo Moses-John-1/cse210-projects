1f0a381241812424e787126bcbfe1a47f6eab34c
Moses-john-1 <mosesgideon.john@gmail.com>
2025-03-08 16:35:19 +0100

    Week 1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,51 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+        do
+        {
+            // Generating Random Numbers
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guessNumber = -1;
+            int guessNumberCount = 0;
+
+            // checking if the guess number is equal magic number
+            while (guessNumber != magicNumber)
+            {
+                Console.Write("What is the guess? ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+
+                guessNumberCount++; // counting time of trial
+
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            // writing out result
+            Console.WriteLine($"You guessed it in {guessNumberCount} tries! ");
+
+            // checking if user want to play again
+            Console.WriteLine("Do you want play again? (yes/no)");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        while (playAgain == "yes");
+
+        Console.WriteLine("Thank you for playing, Goodbye");
+            
+               
     }
 }
+
+

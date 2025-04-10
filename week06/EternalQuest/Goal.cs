f7a9c5845b79a18fb0145f9e4fda69be371a727a
Moses-john-1 <mosesgideon.john@gmail.com>
2025-04-10 22:08:21 +0100

    EternalQuest stubs

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,15 @@
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected string _points;
+
+    public Goal(string name, string description, string points)
+    {
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public abstract string GetDetailsString();
+    public abstract string GetStringRepresentation();
+}
---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,27 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, string points) : base(name, description, points)
+    {
+    }
+
+    public override void RecordEvent()
+    {
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetDetailsString()
+    {
+        return "";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
